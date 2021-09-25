7208617b08e241533c8eeb8f3b4a645046005791
Jack Baron <jackmbaron@gmail.com>
2021-09-25 09:20:35 +0100

    setup redis connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import Redis from 'ioredis'
+import type { Redis as RedisInterface, RedisOptions } from 'ioredis'
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 interface AsyncProxy<T extends {}> {
   get<K extends keyof T>(key: K, defaultValue: T[K]): Promise<T[K]>
@@ -6,8 +9,24 @@ interface AsyncProxy<T extends {}> {
   set<K extends keyof T>(key: K, value: T[K]): Promise<void>
 }
 
+interface Options {
+  /**
+   * Redis Connection
+   */
+  redis: RedisInterface | RedisOptions
+}
+
+/**
+ * Create a new Async Store
+ * @param options Store Options
+ */
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function createStore<T extends {}>(): AsyncProxy<T> {
+export function createStore<T extends {}>(options: Options): AsyncProxy<T> {
+  // eslint-disable-next-line prettier/prettier
+  const redis = options.redis instanceof Redis
+    ? options.redis
+    : new Redis(options.redis)
+
   const proxy: AsyncProxy<T> = {
     async get(key, defaultValue) {
       // TODO
