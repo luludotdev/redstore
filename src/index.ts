b9327d254a419b590cb5cd7fbb4efc000b4c0680
Jack Baron <jackmbaron@gmail.com>
2021-09-25 09:10:36 +0100

    allow setting default value inline

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
 interface AsyncProxy<T extends {}> {
-  get<K extends keyof T>(key: K): Promise<T[K]>
+  get<K extends keyof T>(key: K, defaultValue: T[K]): Promise<T[K]>
+  get<K extends keyof T>(key: K, defaultValue?: T[K]): Promise<T[K] | undefined>
+
   set<K extends keyof T>(key: K, value: T[K]): Promise<void>
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function createStore<T extends {}>(): AsyncProxy<T> {
   const proxy: AsyncProxy<T> = {
-    async get(key) {
+    async get(key, defaultValue) {
       // TODO
       console.log('get', { key })
     },
