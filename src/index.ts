3ac39e878732a451da4e9fea2d47286887ba53e1
Jack Baron <jackmbaron@gmail.com>
2021-09-25 09:05:38 +0100

    skeleton async proxy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export {}
+// eslint-disable-next-line @typescript-eslint/ban-types
+interface AsyncProxy<T extends {}> {
+  get<K extends keyof T>(key: K): Promise<T[K]>
+  set<K extends keyof T>(key: K, value: T[K]): Promise<void>
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function createStore<T extends {}>(): AsyncProxy<T> {
+  const proxy: AsyncProxy<T> = {
+    async get(key) {
+      // TODO
+      console.log('get', { key })
+    },
+
+    async set(key, value) {
+      // TODO
+      console.log('set', { key, value })
+    },
+  }
+
+  return Object.freeze(proxy)
+}
